ec0bc744adc108a4e61adddbf98fdf1c98662b23
Rolando <rolandovillca@gmail.com>
2016-06-09 15:49:46 -0400

    Updating the main component with new objects

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,47 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+  age: number;
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello Angular2 Beware!!</h1>'
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div>
+      <label>id: </label>{{hero.id}}
+    </div>
+    <div>
+      <label>name: </label>
+      <input value="{{hero.name}}" placeholder="name">
+      <br>
+      <label>age: </label>
+      <input [(ngModel)]="hero.age" placeholder="age">
+    </div>
+    `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heores';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+    age: 25
+  }
+}
