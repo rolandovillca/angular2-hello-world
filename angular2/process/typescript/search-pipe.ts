7c1c145692c8fadfb32f73b1b197ed9086f20c43
rolandovillca <rolandovillca@gmail.com>
2016-07-17 07:46:02 -0400

    Adding another example using angular2, as base of a project

---
+++ angular2/process/typescript/search-pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe} from 'angular2/core';
+
+@Pipe ({
+  name: 'find'
+})
+
+export class SearchPipe {
+  transform(pipeData, [pipeModifier]) {
+    return pipeData.filter((eachItem) => {
+      return eachItem['name'].toLowerCase().includes(pipeModifier.toLowerCase()) ||
+            eachItem['reknown'].toLowerCase().includes(pipeModifier.toLowerCase())
+    });
+  }
+}
+
+export class PPP {}
